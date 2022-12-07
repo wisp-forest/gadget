600e6d2ce50b81910d11a3a1d7493aae07f418b3
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-12-07 18:05:37 +0300

    pushed so that modmuss can look

--- src/main/java/io/wispforest/gadget/mixin/owo/OwoUIAdapterMixin.java
+++ src/main/java/io/wispforest/gadget/mixin/owo/OwoUIAdapterMixin.java
@@ -2,6 +2,8 @@ package io.wispforest.gadget.mixin.owo;
 
 import io.wispforest.gadget.client.gui.ComponentEventCounter;
 import io.wispforest.owo.ui.core.OwoUIAdapter;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(OwoUIAdapter.class)
-public class OwoUIAdapterMixin {
-    @Inject(method = "render", at = @At("HEAD"))
+public abstract class OwoUIAdapterMixin implements Drawable, Element {
+    @Inject(method = {"render", "method_25394"}, at = @At("HEAD"))
     private void reset(MatrixStack matrices, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         ComponentEventCounter.reset();
     }
@@ -26,7 +28,7 @@ public class OwoUIAdapterMixin {
         ComponentEventCounter.tally();
     }
 
-    @Inject(method = "render", at = @At("RETURN"))
+    @Inject(method = {"render", "method_25394"}, at = @At("RETURN"))
     private void tally(MatrixStack matrices, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         ComponentEventCounter.tally();
     }
