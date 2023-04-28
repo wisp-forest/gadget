f0c23c5b3469aed1dc9e6dc13a934610d527fd3f
Basique <basiqueevangelist@yandex.ru>
2023-04-28 15:21:27 +0300

    hotfix narrator stuff

--- src/main/java/io/wispforest/gadget/mixin/client/NarratorMixin.java
+++ src/main/java/io/wispforest/gadget/mixin/client/NarratorMixin.java
@@ -1,15 +1,15 @@
 package io.wispforest.gadget.mixin.client;
 
 import com.llamalad7.mixinextras.injector.WrapWithCondition;
-import com.mojang.text2speech.NarratorWindows;
+import com.mojang.text2speech.Narrator;
 import io.wispforest.gadget.Gadget;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = NarratorWindows.class, remap = false)
-public class NarratorWindowsMixin {
-    @WrapWithCondition(method = "tryLoadNative", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+@Mixin(value = Narrator.class, remap = false)
+public interface NarratorMixin {
+    @WrapWithCondition(method = "getNarrator", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"), require = 0)
     private static boolean shhhh(Logger logger, String message, Throwable throwable) {
         return !Gadget.CONFIG.silenceStartupErrors();
     }
