24e15a16662387b74848fbf5d749477fd376807f
Basique <basiqueevangelist@yandex.ru>
2023-07-05 00:48:58 +0300

    fix up context menu screens

--- src/main/java/io/wispforest/gadget/client/gui/ContextMenuScreens.java
+++ src/main/java/io/wispforest/gadget/client/gui/ContextMenuScreens.java
@@ -3,38 +3,24 @@ package io.wispforest.gadget.client.gui;
 import io.wispforest.owo.ui.component.DropdownComponent;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
+import io.wispforest.owo.ui.layers.Layer;
 import io.wispforest.owo.ui.layers.Layers;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.world.SelectWorldScreen;
 
 public class ContextMenuScreens {
+    private static final Layer<Screen, FlowLayout> LAYER = Layers.add(
+        Containers::verticalFlow,
+        instance -> { },
+        SelectWorldScreen.class, MultiplayerScreen.class
+    );
+
     public static void init() {
-        Layers.add(
-            Containers::verticalFlow,
-            instance -> {
-                instance.adapter.rootComponent.child(new CringeComponent()
-                    .id("gadget:cringe"));
-            },
-            SelectWorldScreen.class, MultiplayerScreen.class
-        );
+
     }
 
     public static DropdownComponent contextMenuAt(Screen screen, double mouseX, double mouseY) {
-        var instances = Layers.getInstances(screen);
-        FlowLayout flow = null;
-
-        for (var inst : instances) {
-            if (inst.adapter.rootComponent.childById(CringeComponent.class, "gadget:cringe") != null) {
-                flow = (FlowLayout) inst.adapter.rootComponent;
-                break;
-            }
-        }
-
-        if (flow == null) {
-            throw new IllegalStateException("bruh");
-        }
-
-        return GuiUtil.contextMenu(flow, mouseX, mouseY);
+        return GuiUtil.contextMenu(LAYER.getInstance(screen).adapter.rootComponent, mouseX, mouseY);
     }
 }
